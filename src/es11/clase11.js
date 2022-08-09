83ab7481a4648405f8c746fc89f15140ffda6d06
fabrilez <fabrilez2001@gmail.com>
2022-08-09 17:46:44 -0400

    Import dinamico implementado en ES11 de 2020

---
+++ src/es11/clase11.js
@@ -0,0 +1,8 @@
+//dinamic import
+
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click', async function () {
+    const module = await import('./file.js');
+    module.hello();
+})
